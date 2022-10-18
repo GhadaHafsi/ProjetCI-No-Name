4e7a13366fcc2a3d9e8320c4fcd324cf81c3d09a
GhadaHafsi <ghada.hafsi@esprit.tn>
2022-10-18 16:22:44 +0100

    Trigger build test4

--- src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
+++ src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
@@ -31,7 +31,7 @@ public class Fournisseur implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idFournisseur;
-	//Add NotBlank comment2
+	//Add NotBlank comment2  
 	private String code;
 	private String libelle;
 	@Enumerated(EnumType.STRING)
